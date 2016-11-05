bf7d13f07cbb745b0a7504c6431bffb103b6b1b6
Richard Young <richard.young@utexas.edu>
2016-11-05 09:36:04 -0500

    fix up outcomes / includes names

--- app/views/handlers/profile_handler.py
+++ app/views/handlers/profile_handler.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlalchemy import or_
 from app.models import User, Game
 
@@ -19,21 +20,23 @@ def get_profile_data(session, user_id):
     outcomes = []
 
     for game in games:
-
         if game.winner_id == user_id:
-            outcomes.append({
-                'opponent_name': '',
-                'opponent_id': game.loser_id,
-                'outcome': 'W',
-                'date': game.created_at
-            })
+            opponent_name = game.loser.name
+            opponent_id = game.loser_id
+            outcome = 'W'
         else:
-            outcomes.append({
-                'opponent_name': '',
-                'opponent_id': game.winner_id,
-                'outcome': 'L',
-                'date': game.created_at
-            })
+            opponent_name = game.winner.name
+            opponent_id = game.winner_id
+            outcome = 'L'
+
+        game_date = game.created_at.strftime('%m/%d/%Y %H:%M')
+
+        outcomes.append({
+            'opponent_name': opponent_name,
+            'opponent_id': opponent_id,
+            'outcome': outcome,
+            'date': game_date
+        })
 
     return user, outcomes
 
