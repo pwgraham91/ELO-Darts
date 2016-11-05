37ff3315bc7a9404d757967b04409b6f77135b93
Richard Young <richard.young@utexas.edu>
2016-11-05 04:36:04 -0500

    more requirements / add-game submits a game to db

--- app/static/js/addgame.js
+++ app/static/js/addgame.js
@@ -2,13 +2,23 @@ console.log('add game')
 // this isn't hooked up to anything but it works
 
 $(document).ready(function() {
+	$('.modal-button').click(function(event) {
+	event.preventDefault();
+		$('.modal').addClass('is-active');
+	});
+
+	$('#modal-cancel, #modal-delete').click(function(event) {
+		event.preventDefault();
+	$('.modal').removeClass('is-active');
+	});
+
 	$('#add-game').click(function () {
 		$.ajax({
 			type: "POST",
 			url: '/games/add',
 			data: JSON.stringify({
-				winner_id: 1,
-				loser_id: 2
+				winner_id: $('select.is-success option:selected').val(),
+				loser_id: $('select.is-danger option:selected').val()
 			}),
 			success: function (data) {
 				console.log(data)
