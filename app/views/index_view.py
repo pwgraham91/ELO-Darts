0c9b416e9c2b5d2a0b7d6a38b7ce3e94437ef119
Richard Young <richard.young@utexas.edu>
2016-11-05 00:17:10 -0500

    initial table view

--- app/views/index_view.py
+++ app/views/index_view.py
@@ -1,12 +1,19 @@
 import flask
-
-from app import app
+from app import app, db
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = flask.g.user
+
+    session = db.session
+
+    current_user = flask.g.user
+
+    active_users = session.query(User).all()
+
     return flask.render_template('index.html',
                                  title='Home',
-                                 user=user)
+                                 current_user=current_user,
+                                 active_users=active_users)
