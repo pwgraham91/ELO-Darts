f85c592eb6b61f29d2a505057c0eedd43ca8be2c
Richard Young <richard.young@utexas.edu>
2016-11-17 09:26:34 -0600

    only winner can submit, disable button (#34)

--- app/static/js/add_game.js
+++ app/static/js/add_game.js
@@ -6,36 +6,29 @@ $(document).ready(function() {
 		$('.modal').addClass('is-active');
 	});
 
-	$('#modal-cancel, #modal-delete').click(function(event) {
-		event.preventDefault();
+	var clearModal = function() {
 		$('select.select-winner').prop('selectedIndex',0);
 		$('select.select-loser').prop('selectedIndex',0);
 		$('select option').removeClass('hidden');
 		$('.modal').removeClass('is-active');
 		$('#add-game').attr('disabled', 'disabled');
+	}
+
+	$('#modal-cancel, #modal-delete').click(function(event) {
+		event.preventDefault();
+		clearModal();
 	});
 
-	$('select.select-winner').change(function(event) {
-		var val = $('select.select-winner option:selected').val();
-		$('select.select-loser option').removeClass('hidden');
-		$('select.select-loser option[value="' + val + '"]').addClass('hidden');
-
+	$('select.select-loser').change(function(e) {
 		if($('select.select-winner').val() && $('select.select-loser').val()) {
 			$('#add-game').removeAttr('disabled', 'disabled');
 		}
 	});
 
-	$('select.select-loser').change(function(event) {
-		var val = $('select.select-loser option:selected').val();
-		$('select.select-winner option').removeClass('hidden');
-		$('select.select-winner option[value="' + val + '"]').addClass('hidden');
+	$('#add-game').click(function (e) {
+		var $this = jQuery(this);
+		$this.attr('disabled', 'disabled');
 
-		if($('select.select-winner').val() && $('select.select-loser').val()) {
-			$('#add-game').removeAttr('disabled', 'disabled');
-		}
-	});
-
-	$('#add-game').click(function () {
 		$.ajax({
 			type: "POST",
 			url: '/games/add',
@@ -45,21 +38,15 @@ $(document).ready(function() {
 			}),
 			success: function (data) {
 				console.log(data);
-				$('select.select-winner').prop('selectedIndex',0);
-				$('select.select-loser').prop('selectedIndex',0);
-				$('select option').removeClass('hidden');
-				$('.modal').removeClass('is-active');
-				$('#add-game').attr('disabled', 'disabled');
+
+				clearModal();
 
 				window.location.reload();
 			},
 			error: function (data) {
 				console.log(data);
-				$('select.select-winner').prop('selectedIndex',0);
-				$('select.select-loser').prop('selectedIndex',0);
-				$('select option').removeClass('hidden');
-				$('.modal').removeClass('is-active');
-				$('#add-game').attr('disabled', 'disabled');
+
+				clearModal();
 			},
 			contentType: 'application/json'
 		});
